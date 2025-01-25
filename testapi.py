67f972f93f2555eaa0fbff621724fb845ceb5aa0
R0ober0t <robertoprisan06@gmail.com>
2025-01-25 11:28:38 +0200

    hi

--- testapi.py
+++
@@ -1,7 +0,0 @@
-from requests import post
-
-api_key = "743c1f4e-1dfb-4f39-828a-a2e96343b8b4"
-url  = "http://localhost:8000/delete-unverified-users"
-
-response = post(url, json={"api_key": api_key})
-print(response.json())
